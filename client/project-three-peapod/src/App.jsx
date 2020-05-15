2028231273c4071ac94aaa85321bc71fcc522e5b
jp <y.polanco12@gmail.com>
2020-05-15 13:13:25 -0400

    -user Auth done -signIn component done -signOut component done -signUp component done

--- client/project-three-peapod/src/App.jsx
+++ client/project-three-peapod/src/App.jsx
@@ -1,12 +1,14 @@
-
-import React, { Component } from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import { verifyUser } from './services/user'
-import ProductsContainer from './components/ProductsContainer'
-import Product from './components/Product'
-import ProductEdit from './components/ProductEdit'
-import './App.css'
-import Footer from './components/shared/Footer'
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { verifyUser } from "./services/user";
+import ProductsContainer from "./components/ProductsContainer";
+import Product from "./components/Product";
+import ProductEdit from "./components/ProductEdit";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import SignOut from "./components/SignOut";
+import "./App.css";
+import Footer from "./components/shared/Footer";
 
 export default class App extends Component {
   constructor() {
@@ -25,10 +27,14 @@ export default class App extends Component {
 
   // setUser = user => this.setState({ user })
   // clearUser = () => this.setState({ user })
+  setUser = (user) => this.setState({ user });
+  clearUser = () => this.setState({ user: null });
 
   render() {
     // const { setUser, clearUser } = this
     // const { user } = this.state
+    const { setUser, clearUser } = this;
+    const { user } = this.state;
 
     return (
       <>
@@ -37,13 +43,31 @@ export default class App extends Component {
             <Route exact path="/products">
               <ProductsContainer />
             </Route>
-            <Route exact path="/product/:id" component={Product}>
-            </Route>
+            <Route exact path="/product/:id" component={Product}></Route>
             <Route exact path="/products/:id/edit" component={ProductEdit} />
+            <Route
+              exact
+              path="/signIn"
+              render={(props) => (
+                <SignIn setUser={setUser} history={props.history} />
+              )}
+            />
+            <Route exact path="/signUp" component={SignUp} />
+            <Route
+              exact
+              path="/signOut"
+              render={(props) => (
+                <SignOut
+                  user={user}
+                  clearUser={clearUser}
+                  history={props.history}
+                />
+              )}
+            />
           </Switch>
         </div>
         <Footer />
       </>
-    )
+    );
   }
 }
