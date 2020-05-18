5d7c545e57e8a01c931bcfbc38566873f719abe9
Claire Buettner <claire@Claires-MacBook-Air.local>
2020-05-18 16:28:06 -0400

    updated home and footer styling

--- client/project-three-peapod/src/components/Home.jsx
+++ client/project-three-peapod/src/components/Home.jsx
@@ -22,8 +22,44 @@ const Home = () => {
       <div className="cover">
         <h2>Shop for thousands of grocery items for your home or business</h2>
         <h3>Grocery shop from anywhere.</h3>
-        <p>Delivery on your schedule</p>
-      </div>
+        <p>Delivery on your schedule.</p>
+      
+
+      <form>
+        <div className="radio">
+          <label>
+            <input 
+            type="radio" 
+            value="Home" 
+            name="Home" 
+            checked={"true"} 
+            className="radio-home" 
+            /> 
+            Home
+          </label>
+            <label>
+            <input 
+            type="radio" 
+            value="Business" 
+            name="Business" 
+            checked={""}
+            className="radio-business"
+            />
+            Business 
+          </label>
+        </div>
+      </form>
+      <form className="search-home">
+            <input
+                className="search-input"
+                value="Enter Zip"
+                name="Search"
+                placeholder="Search"
+                type="text"
+                />
+                <button><strong>Browse Now</strong></button>
+        </form>
+        </div>
     <Footer />
   </>
   )
