9167853ff257763f674fc21bec56dad91e3872b5
Claire Buettner <claire@Claires-MacBook-Air.local>
2020-05-20 19:06:10 -0400

    edited styling on product card and products container edited home page styling edited shopping list styling

--- client/project-three-peapod/src/components/ShoppingList.jsx
+++ client/project-three-peapod/src/components/ShoppingList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./ShoppingList.css"
+import Layout from "./shared/Layout"
 
 function ShoppingList() {
     return (
-        <>
-        <div>
-           <h1>SHOPPING LIST</h1> 
+        <Layout>
+        <div className="shopping-bar">
+           <h3>Shopping List</h3> 
         </div>
+
+
+
         <div className="createNew">
             <p>Create New List</p>
             <form className="essentials">
@@ -20,11 +24,12 @@ function ShoppingList() {
                 <button className="create-button"><strong>Create</strong></button>
              </form>
         </div>   
+
         <div className="create-a-list">
             <h2>Create a List</h2>
             <p>Create a new list by entering a list name and clicking the Create button in the left panel</p>
             </div> 
-        </>
+        </Layout>
     )
 }
 
