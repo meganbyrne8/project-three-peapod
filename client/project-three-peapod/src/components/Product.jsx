716cf1f01189b3a2f7be1d6f15c11ff2aadc9994
Megan Byrne <jameskaiser@Jamess-MacBook-Air-2.local>
2020-05-20 19:05:50 -0400

    Imported all relevant thumbnails added nutrition logo added styles to nutrition section added substitute div added checkbox w text for substitute div fixed image overflow

--- client/project-three-peapod/src/components/Product.jsx
+++ client/project-three-peapod/src/components/Product.jsx
@@ -6,6 +6,10 @@ import ShoppingList from "./ProductItemImages/ShoppingList.png"
 import Rating from "./ProductItemImages/Rating.png"
 import Small_Chat from "./ProductItemImages/Small_Chat.png"
 import nutrition_reviews from "./ProductItemImages/nutrition_reviews.png"
+import nutritionLogo from "./ProductItemImages/nutritionLogo.png"
+import AddToCart from "./ProductItemImages/AddToCart.png"
+import tomatoesThumbnail from "./ProductItemImages/tomatoesThumbnail.png"
+import checkedBox from "./ProductItemImages/checkedBox.png"
 import "./Product.css"
 
 export default class Product extends Component {
@@ -22,7 +26,6 @@ export default class Product extends Component {
       aisle: '',
       _id: ''
     }
-
   }
 
 
@@ -53,6 +56,7 @@ export default class Product extends Component {
               <div className="main-info-thumbnail">
                 <img src={this.state.image} />
               </div>
+
               <div className="stack-icons-info">
                 <div className="icons-div">
                   <img src={ShoppingList} className="shopping-icon" />
@@ -65,43 +69,135 @@ export default class Product extends Component {
               </div>
             </div>
             <div className="price-button-div-item">
-              <span className="price-lower-item">${this.props.price}</span>
-              <button className="add-to-cart-item">Add To Cart</button>
+              <div className="price-button-div-item-text">
+                <span className="price-lower-item">$ {this.state.price}</span>
+                <p>1.75 lb packaging | 0.46 / oz</p>
+              </div>
+              <img src={AddToCart} />
             </div>
+
             <div className="edit-delete-div">
               <Link to={`/products/${this.state._id}/edit`} className="edit-link">Edit</Link>
               <button className="delete-button" onClick={() => deleteProduct(this.state._id)}>Delete</button>
             </div>
+
             <div className="nutrition-div">
               <div className="nutrition-recipe-banner">
                 <img src={nutrition_reviews} />
               </div>
+              <div className="nutrition-logo">
+                <img src={nutritionLogo} />
+              </div>
               <div className="info-for-nutrition">
-                Some more text around this div
-          </div>
+                <div className="nutrition-split-div">
+                  <h3 className="nutrition-title">
+                    Nutrition
+                  </h3>
+                  <div className="nutrition-breakdown-div">
+                    <div className="single-nutrition-breakdown-div">
+                      <h3>41</h3>
+                      <h4>CALORIES</h4>
+                    </div>
+                    <div className="single-nutrition-breakdown-div">
+                      <h3>0.1g</h3>
+                      <h4>SAT FAT</h4>
+                    </div>
+                    <div className="single-nutrition-breakdown-div">
+                      <h3>11mg</h3>
+                      <h4>SODIUM</h4>
+                    </div>
+                    <div className="single-nutrition-breakdown-div">
+                      <h3>6g</h3>
+                      <h4>SUGARS</h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="substitute-item-div">
+                  <h3 className="substitute-item-title">Substitute Item</h3>
+                  <div className="substitute-image">
+                    <img src={tomatoesThumbnail} className="beefsteak-tomatoes-thumbnail" />
+                    <div className="beefsteak-tomatoes-substitute-text">
+                      <h6>Tomatoes Beefsteak</h6>
+                      <p>2 ct pkg</p>
+                    </div>
+                  </div>
+                  <div className="substitute-checkbox">
+                    <img src={checkedBox} />
+                    <p>If unavailable, substitute with this item</p>
+                  </div>
+                </div>
+              </div>
             </div>
+
           </div>
 
 
           <div className="y-axis-main-section">
             <div className="suggestions-div">
               <div className="suggestins-div-title">
-                <h4>You Might Like</h4>
+                <h3>You Might Like</h3>
               </div>
+
               <div className="listed-suggestions">
                 <div className="single-main-info-div">
                   <div className="single-suggested-image-div">
-                    Image here
-                </div>
+                    <img src={tomatoesThumbnail} className="beefsteak-tomatoes-thumbnail" />
+                  </div>
                   <div className="single-suggested-info-div">
                     Weight, title here
                 </div>
                 </div>
                 <div className="single-suggested-button-price-div">
-                  price
-                  <button className="single-suggested-add-button">Add to Cart</button>
+                  <p>8.99</p>
+                  <img src={AddToCart} className="singular-add-to-cart" />
                 </div>
               </div>
+
+              <div className="listed-suggestions">
+                <div className="single-main-info-div">
+                  <div className="single-suggested-image-div">
+                    <img src={tomatoesThumbnail} className="beefsteak-tomatoes-thumbnail" />
+                  </div>
+                  <div className="single-suggested-info-div">
+                    Weight, title here
+                </div>
+                </div>
+                <div className="single-suggested-button-price-div">
+                  <p>8.99</p>
+                  <img src={AddToCart} className="singular-add-to-cart" />
+                </div>
+              </div>
+
+              <div className="listed-suggestions">
+                <div className="single-main-info-div">
+                  <div className="single-suggested-image-div">
+                    <img src={tomatoesThumbnail} className="beefsteak-tomatoes-thumbnail" />
+                  </div>
+                  <div className="single-suggested-info-div">
+                    Weight, title here
+                </div>
+                </div>
+                <div className="single-suggested-button-price-div">
+                  <p>8.99</p>
+                  <img src={AddToCart} className="singular-add-to-cart" />
+                </div>
+              </div>
+
+              <div className="listed-suggestions">
+                <div className="single-main-info-div">
+                  <div className="single-suggested-image-div">
+                    <img src={tomatoesThumbnail} className="beefsteak-tomatoes-thumbnail" />
+                  </div>
+                  <div className="single-suggested-info-div">
+                    Weight, title here
+                </div>
+                </div>
+                <div className="single-suggested-button-price-div">
+                  <p>8.99</p>
+                  <img src={AddToCart} className="singular-add-to-cart" />
+                </div>
+              </div>
+
             </div>
           </div>
 
