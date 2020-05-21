8e3cc17e6dd1318b0aa92bf0099ed4dcdd565a4f
jp <y.polanco12@gmail.com>
2020-05-21 15:29:56 -0400

    mobile nav

--- client/project-three-peapod/src/components/shared/navigation/Nav.jsx
+++ client/project-three-peapod/src/components/shared/navigation/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./stop-and-shop-logo.jpg";
 import { FiUser } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Cart from "./Cart";
 import "./Nav.css";
 import Dropdown from "./DropdownShop";
@@ -45,38 +46,43 @@ class Nav extends Component {
 
     return (
       <div className="nav">
-        <div>
-          <img src={logo} alt="stop-shop-logo" className="logo" />
+        <div className="burger">
+        <GiHamburgerMenu />
         </div>
-        <div className="drop-down">
-          <h3>
-            <DropdownBrowse
-              aisles={this.state.aisles.map((aisle, index) => (
-                <div>
-                  <li id={aisle._id} key={index}>
-                    {aisle.aisle}
-                  </li>
-                </div>
-              ))}
-            />
-          </h3>
-          <h3 className="shop">
-            <Dropdown />
-            {/* <i className="arrow down"></i> */}
-          </h3>
+        <div className="mobile-container">
+          <div>
+            <img src={logo} alt="stop-shop-logo" className="logo" />
+          </div>
+          <div className="drop-down">
+            <h3>
+              <DropdownBrowse
+                aisles={this.state.aisles.map((aisle, index) => (
+                  <div>
+                    <li id={aisle._id} key={index}>
+                      {aisle.aisle}
+                    </li>
+                  </div>
+                ))}
+              />
+            </h3>
+            <h3 className="shop">
+              <Dropdown />
+              {/* <i className="arrow down"></i> */}
+            </h3>
+          </div>
+          <div className="search">
+            <form>
+              <Suggestions />
+            </form>
+          </div>
         </div>
-        <div className="search">
-          <form>
-            <Suggestions />
-          </form>
-        </div>
+        <div className="guest-container">
         <div className="sign-out">
           <p>
-            All set? 
+            All set?
             <Link to="/signOut"> Sign Out </Link>
           </p>
         </div>
-        <div className="guest-container">
           <span className="person">
             <FiUser />
             <p className="guest">Guest</p>
--- client/project-three-peapod/src/components/shared/navigation/SecondaryNav.jsx
+++ client/project-three-peapod/src/components/shared/navigation/SecondaryNav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./SecondaryNav.css";
+import { FiTruck } from "react-icons/fi";
+import { AiOutlineClockCircle } from "react-icons/ai";
 
 export default function SecondaryNav() {
   return (
@@ -29,17 +31,27 @@ export default function SecondaryNav() {
         </Link>
       </ul>
       <div className="inbeded">
-        <Link>
-          <li>Delivery</li>
-        </Link>
-        <span className="spans">to</span>
-        <Link>
-          <li>10035</li>
-        </Link>
-        <span className="spans">|</span>
-        <Link>
-          <li>Select a Time</li>
-        </Link>
+        <div className="delivery">
+          <span className="truck">
+            <FiTruck />
+          </span>
+          <Link>
+            <li>Delivery</li>
+          </Link>
+          <span className="spans">to</span>
+          <Link>
+            <li>10035</li>
+          </Link>
+        </div>
+        <div className="time">
+          <span className="clock">
+            <AiOutlineClockCircle />
+          </span>
+          <span className="span2">|</span>
+          <Link>
+            <li>Select a Time</li>
+          </Link>
+        </div>
       </div>
     </div>
   );
