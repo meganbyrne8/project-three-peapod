5f35060e62e431395edf72c3d0d2cded85e2d36d
Claire Buettner <claire@Claires-MacBook-Air.local>
2020-05-21 08:52:17 -0400

    made styingling adjustments to shopping list fixed spelling error on nav

--- client/project-three-peapod/src/components/ShoppingList.jsx
+++ client/project-three-peapod/src/components/ShoppingList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./ShoppingList.css"
 import Layout from "./shared/Layout"
+import create from "./create.png"
+import BackMenuButton from "./ProductItemImages/BackMenuButton.png"
+
 
 function ShoppingList() {
     return (
         <Layout>
         <div className="shopping-bar">
+           <div className="click-back-button">
+              <img src={BackMenuButton} className="back-button" />
+            </div>
            <h3>Shopping List</h3> 
         </div>
-
-
 
         <div className="createNew">
             <p>Create New List</p>
@@ -21,13 +25,16 @@ function ShoppingList() {
                 placeholder="Search"
                 type="text"
                 />
-                <button className="create-button"><strong>Create</strong></button>
+                <img src={create} alt="create" className="create" />
              </form>
         </div>   
 
         <div className="create-a-list">
             <h2>Create a List</h2>
-            <p>Create a new list by entering a list name and clicking the Create button in the left panel</p>
+            <hr className="line"/>
+            <br/>
+            <p>Create a new list by entering a list name and </p>
+            <h4>clicking the Create button in the left panel</h4>
             </div> 
         </Layout>
     )
